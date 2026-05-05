313034fe33d7ec657d112d9eff51c71bc8d7c0a1
guest-fgx4 <noobdev341@proton.me>
2026-05-05 18:48:58 -0300

    Update project structure

---
+++ ed09.c
@@ -0,0 +1,55 @@
+int main(void)
+{
+  int opcao = 0;
+
+  printf("Metodo Main\n");
+
+  do
+  {
+    printf("Entrar com opcao para selecionar metodo (1-10)\n");
+
+    scanf("%d", &opcao);
+
+    switch (opcao)
+    {
+    case 1:
+      metodo_011();
+      break;
+    case 2:
+      metodo_012();
+      break;
+    case 3:
+      metodo_013();
+      break;
+    case 4:
+      metodo_014();
+      break;
+    case 5:
+      metodo_015();
+      break;
+    case 6:
+      metodo_016();
+      break;
+    case 7:
+      metodo_017();
+      break;
+    case 8:
+      metodo_018();
+      break;
+    case 9:
+      metodo_019();
+      break;
+    case 10:
+      metodo_020();
+      break;
+    default:
+      printf("%s\n", "Metodo invalido, Selecionar metodos de 1 ate 10");
+      printf("%s\n", "Ou pressione 0 para sair");
+      break;
+    }
+  } while (opcao != 0);
+
+  printf("Apertar ENTER para sair do programa main.");
+  getchar();
+  return 0;
+}
